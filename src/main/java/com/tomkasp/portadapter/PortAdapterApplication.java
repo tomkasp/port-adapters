0e2d31fe54e0b71c6ec5620d512641325e327d35
tomkasp@gmail.com <tomkasp@gmail.com>
2020-04-02 21:20:59 +0200

    initial commit

---
+++ src/main/java/com/tomkasp/portadapter/PortAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.tomkasp.portadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tomkasp/portadapter/PortAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomkasp.portadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
